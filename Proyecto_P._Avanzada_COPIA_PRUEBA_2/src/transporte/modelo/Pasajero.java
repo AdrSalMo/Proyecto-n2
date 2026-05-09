ccfe0798eebc26eefe4d6d48c93c7222238cdcf1
AdrSalMo <asans2059@gmail.com>
2026-05-08 22:05:48 -0400

    Pasajero extends

--- Proyecto_P._Avanzada_COPIA_PRUEBA_2/src/transporte/modelo/Pasajero.java
+++ Proyecto_P._Avanzada_COPIA_PRUEBA_2/src/transporte/modelo/Pasajero.java
@@ -2,20 +2,17 @@ package transporte.modelo;
 
 import java.io.Serializable;
 
-public class Pasajero implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    private String nombre;
-    private String rut;
+public class Pasajero extends Persona implements Serializable {
+    private static final long serialVersionUID = 1L; // Buena práctica para Serializable
     private int id;
 
     public Pasajero(String nombre, String rut, int id) {
-        this.nombre = nombre;
-        this.rut = rut;
+        super(nombre, rut); // Llama al constructor de Persona (Clase base)
         this.id = id;
     }
 
+    @Override
     public String toString() {
-        return nombre + " (" + rut + ")";
+        return getNombre() + " (" + getRut() + ") - ID: " + id;
     }
 }
